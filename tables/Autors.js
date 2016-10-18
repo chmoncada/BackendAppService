961fd85541d751a0295f596164bad92d9f094c4b
Charles Moncada <charles.moncada@emoshiapps.com>
2016-10-18 16:00:44 -0500

    Crear tabla de Autors, sin cambiar el modo del schema ni asignar autenticacion

--- tables/Autors.js
+++ tables/Autors.js
@@ -11,4 +11,12 @@ table.columns = {
     "secondName": "string"
 };
 
+/*
+* Permisos de acceso a la tabla
+* */
+
+
+table.read.access = 'anonymous';
+table.update.access = 'disable';
+
 module.exports = table;
