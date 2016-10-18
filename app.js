400003987389c15a6876697c53d4b3f159412522
Charles Moncada <charles.moncada@emoshiapps.com>
2016-10-18 11:22:56 -0500

    Version inicial del servicio

---
+++ app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by charlesmoncada on 18/10/16.
+ */
+
+var express = require("express"),
+    azuremobileapps = require("azure-mobile-apps");
+
+var app = express(),
+    mobile = azuremobileapps();
+
+mobile.tables.add("./tables");
+
+mobile.api.add("./api");
+
+app.use(mobile);
+
+app.listen(process.env.PORT || 3000);
